434329fc80460a8a5ad308c975aa0045636a953f
Moksleivis <Moksleivis@KITM.local>
2020-09-15 10:48:45 +0300

    endofclass

--- CSharpClass/CSharpClass/Program.cs
+++ CSharpClass/CSharpClass/Program.cs
@@ -6,7 +6,38 @@ namespace CSharpClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type 1-7 to get a weekday:");
+
+            bool validInput = int.TryParse(Console.ReadLine(), out int number);
+            if (validInput)
+            {
+                if (0 < number && number < 8)
+                {
+                    WeekDays day = (WeekDays)number;
+                    Console.WriteLine("That's " + day);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong number, should be (1-7)");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a number");
+            }
         }
     }
+
+    public enum WeekDays
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
 }
+
+
