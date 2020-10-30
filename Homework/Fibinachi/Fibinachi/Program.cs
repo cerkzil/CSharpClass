f9936c3423f346502c932f85647019737856d763
cerkzil <39460996+cerkzil@users.noreply.github.com>
2020-10-30 23:14:35 +0200

    added old fibananchi hw

---
+++ Homework/Fibinachi/Fibinachi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibinachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How long do you want Fibinachi sequance to go?");
+            int b = int.Parse(Console.ReadLine());
+            int[] array = new int[b+1];
+
+            if (b > 0) { array[0] = 0; Console.Write(array[0]); }
+            if (b > 1) { array[1] = 1; Console.Write(", " + array[1]); }
+            if (b >= 2)
+            {
+                for (int i = 2; i < b; i++)
+                {
+                    array[i] = array[i - 1] + array[i - 2];
+                    Console.Write(", " + array[i]);
+                }
+            }
+        }
+    }
+}
